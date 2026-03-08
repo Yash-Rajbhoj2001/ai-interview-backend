69280caf42679976d9fa7b27bfc015f963a2ccb8
Yash-Rajbhoj2001 <yashrajbhoj2331@gmail.com>
2026-03-08 18:05:56 +0530

    feat: add first_name and last_name support in user system

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -5,17 +5,31 @@ from .models import User
 # User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
+
     password = serializers.CharField(write_only=True)
+    name = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        # fields = ["username", "email", "password"]
-        fields = ["email", "password"]
+        fields = ["name", "email", "password"]
 
     def create(self, validated_data):
+
+        full_name = validated_data.pop("name")
+
+        first_name = ""
+        last_name = ""
+
+        if full_name:
+            parts = full_name.split(" ")
+            first_name = parts[0]
+            last_name = " ".join(parts[1:]) if len(parts) > 1 else ""
+
         user = User.objects.create_user(
-            # username=validated_data["username"],
             email=validated_data["email"],
-            password=validated_data["password"]
+            password=validated_data["password"],
+            first_name=first_name,
+            last_name=last_name
         )
+
         return user
